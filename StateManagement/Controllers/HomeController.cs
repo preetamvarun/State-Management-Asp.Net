74d7ee0e8acdf3dfe7a345f4bff8d115a47764c3
PREETAM <preetamvarun99@gmail.com>
2024-09-22 13:49:25 +0100

    Practiced cookies

--- StateManagement/Controllers/HomeController.cs
+++ StateManagement/Controllers/HomeController.cs
@@ -100,6 +100,67 @@ namespace StateManagement.Controllers
 
         #endregion
 
+        #region PracticeCookies
+
+
+        #region CookieNotes
+        /*
+         * Cookie is a small piece of information that is stored in the client computer 
+         * To create a cookie we are going to use HttpCookie Class.
+         * Then assign value to the cookie object
+         * To add the cookie in the client side computer : Response.Cookies.Add(your_cookie);
+         * Be default, Every cookie is an in-memory cookie. That means cookie will be destroyed after the browser has been closed.
+         * If you want to make the cookie persistant : your_cookie.Expires = DateTime.Now.AddDays(2);
+         * To read a cookie : HttpCookie your_cookie = Request.Cookies["login cookie"];
+         * cookies can store only string data types. 
+         * Cookie is accessible in all the pages
+        */
+        #endregion
+
+        #region CookieDrawBacks
+        /*
+         * We can only store 50 cookies per website
+         * If we add a 51st cookie 1st cookie will be deleted.
+         * Cookies can store max 4KB of data
+         * Cookies are not secured as they are stored in the client computer
+         * End users can disable or delete the cookies 
+        */
+        #endregion
+
+        [HttpGet]
+        public ViewResult FavouriteSingerDetails()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public RedirectToRouteResult FavouriteSingerDetails(string sname, string sethnicity, int sage)
+        {
+            // Create a cookie here
+            HttpCookie Singer_Cookie = new HttpCookie("Singer_Cookie");
+
+            // Store the values in a cookie
+            Singer_Cookie["Name"] = sname;
+            Singer_Cookie["Country"] = sethnicity;
+
+            // This is because cookeis can only store string datatypes
+            Singer_Cookie["Age"] = sage.ToString();
+
+            // Make this cookie a persistant cookie
+            Singer_Cookie.Expires = DateTime.Now.AddDays(1);
+
+
+            // Finally, store this cookie in the client's browser
+            Response.Cookies.Add(Singer_Cookie);
+
+
+            return RedirectToAction("DisplaySinger", "Test");
+        }
+
+
+        #endregion
+
+
 
     }
 }
---
+++ StateManagement/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using static System.Net.Mime.MediaTypeNames;
+using System.Web.UI.WebControls;
+
+namespace StateManagement.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ViewResult DisplaySinger()
+        {
+            return View("~/Views/Test/DisplaySinger.cshtml");
+        }
+    }
+}
